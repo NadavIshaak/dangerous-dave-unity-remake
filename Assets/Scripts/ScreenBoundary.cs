3eb1ad0be7b2a549f886258b7b153e19c2c34341
NadavIshaak <nako354@gmail.com>
2025-01-16 01:08:26 +0200

    finished cameras in staage 2

---
+++ Assets/Scripts/ScreenBoundary.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Cinemachine;
+
+public class ScreenBoundary : MonoBehaviour
+{
+     [SerializeField] private int currentScreenIndex;
+     [SerializeField] private CinemachineSplineCart dollyCart;
+    private float boundaryX; // The X position of this boundary in world space
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!other.CompareTag("Player")) return;
+        var playerX = other.transform.position.x;
+        boundaryX = transform.position.x;
+        if (playerX > boundaryX)
+        {
+            // The player crossed this boundary from left to right, so go to the NEXT screen
+            dollyCart.SplinePosition = currentScreenIndex;
+        }
+        else
+        {
+            // The player crossed from right to left, so go to the PREVIOUS screen
+            dollyCart.SplinePosition = currentScreenIndex + 1;
+        }
+    }
+}
